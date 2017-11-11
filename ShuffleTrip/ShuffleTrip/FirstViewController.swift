296eeca7a4b2722ec9e9761e269a2c2b75d5189b
Loic Verrall <loic.verrall@gmail.com>
2017-11-11 14:31:25 +0000

    Blank tab bar app created

---
+++ ShuffleTrip/ShuffleTrip/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ShuffleTrip
+//
+//  Created by Loic Verrall on 11/11/2017.
+//  Copyright © 2017 ASE-ESRS. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
